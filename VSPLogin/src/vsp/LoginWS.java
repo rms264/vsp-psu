9a50ec46093b305428e54dd2be0a391eb5177900
Rob Stratton <rms264@psu.edu>
2012-09-20 20:30:29 -0400

    Initial Commit For VSP Login Project

---
+++ VSPLogin/src/vsp/LoginWS.java
@@ -0,0 +1,18 @@
+package vsp;
+
+
+public class LoginWS
+{
+	public static LoginBean login(LoginBean bean)
+	{
+		String username = bean.getUsername();
+		String password = bean.getPassword();
+		
+		
+		//Make Web Service Call
+		//If username and password are valid then populate bean with data
+		//and return
+		
+		return bean;
+	}
+}
