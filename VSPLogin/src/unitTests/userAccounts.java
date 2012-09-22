2ec35dfcd1d90887220dfcf869bd550dda6ba3dd
Jason <jmt9n@alumni.virginia.edu>
2012-09-22 17:55:33 -0400

    Created unit tests for Section 3.1.1

--- VSPLogin/src/unitTests/userAccounts.java
+++ VSPLogin/src/unitTests/userAccounts.java
@@ -16,8 +16,86 @@ public class userAccounts
 	}
 
 	@Test
-	public void test()
+	public void authenticationTest()
 	{
-		fail("Not yet implemented");
+		Assert.fail();
+	}
+	
+	@Test
+	public void createAccount()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithDuplicateUserName()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithBlankPassword()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithInvalidEmail()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithDuplicateEmail()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordLessThan8Characters()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordMoreThan64Characters()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordMissingUppercaseCharacter()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordMissingLowercaseCharacter()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordMissingNumericCharacter()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithPasswordMatchingUserName()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithoutSecurityQuestion()
+	{
+		Assert.fail();
+	}
+	
+	@Test
+	public void denyNewAccountWithoutSecurityAnswer()
+	{
+		Assert.fail();
 	}
 }
