b1e188abbdae7a99e14555e66e95f6aaf16baced
Jason <jmt9n@alumni.virginia.edu>
2012-09-22 17:43:42 -0400

    added user account test cases

---
+++ VSPLogin/src/unitTests/userAccounts.java
@@ -0,0 +1,23 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class userAccounts
+{
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+}
