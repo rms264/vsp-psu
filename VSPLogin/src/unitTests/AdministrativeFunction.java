f354f6db5bd2752b8254751178074b4642fedcec
Jason <jmt9n@alumni.virginia.edu>
2012-09-29 23:21:05 -0400

    Updated getUserList test.  It passes now.

--- VSPLogin/src/unitTests/AdministrativeFunction.java
+++ VSPLogin/src/unitTests/AdministrativeFunction.java
@@ -2,37 +2,61 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
-public class AdministrativeFunction {
+import vsp.VspServiceProvider;
 
+public class AdministrativeFunction
+{
+	private final VspServiceProvider vsp = new VspServiceProvider();
+	
 	@Test
 	@Order(order=1)
-	public void getAdminAccess() {
+	public void getAdminAccess()
+	{
 		fail("Not yet implemented");		
 	}
 
 	@Test
 	@Order(order=2)
-	public void getUserList() {
-		fail("Not yet implemented");		
+	public void getUserList()
+	{
+		try
+		{
+			// the default traders in the database include 'test' and 'test1'
+			List<String> traders = vsp.getTraders();
+			Assert.assertTrue(traders.size() >= 2);
+			Assert.assertTrue(traders.contains("test"));
+			Assert.assertTrue(traders.contains("test1"));
+		}
+		catch (Exception e)
+		{
+			Assert.fail();
+		}
 	}
 	
 	@Test
 	@Order(order=3)
-	public void removeUser() {
+	public void removeUser()
+	{
 		fail("Not yet implemented");		
 	}
 	
 	@Test
 	@Order(order=4)
-	public void changeEmail() {
+	public void changeEmail()
+	{
 		fail("Not yet implemented");		
 	}
 	
 	@Test
 	@Order(order=5)
-	public void changePassword() {
+	public void changePassword()
+	{
 		fail("Not yet implemented");		
 	}
 }
