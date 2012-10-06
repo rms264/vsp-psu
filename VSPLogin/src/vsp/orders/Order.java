bf2422c3d21dd021a64c4057a57592a388372ab4
Jason <jmt9n@alumni.virginia.edu>
2012-10-06 13:51:28 -0400

    Implemented.

--- VSPLogin/src/vsp/orders/Order.java
+++ VSPLogin/src/vsp/orders/Order.java
@@ -1,6 +1,89 @@
 package vsp.orders;
 
+import vsp.dataObject.Stock;
+import vsp.utils.Enumeration.*;
+
 public final class Order
 {
-	// TODO: populate
+	private final String id;
+	private final String userName;
+	private final Stock stock;
+	private final OrderAction action;
+	private final float quantity;
+	private final OrderType type;
+	private final double limitPrice;
+	private final double stopPrice;
+	private final TimeInForce timeInForce;
+	private OrderState state;
+	
+	public Order(String id, String userName, Stock stock, OrderAction action, 
+			float quantity, OrderType type, double limitPrice, double stopPrice,
+			TimeInForce timeInForce, OrderState state)
+	{
+		this.id = id;
+		this.userName = userName;
+		this.stock = stock;
+		this.action = action;
+		this.quantity = quantity;
+		this.type = type;
+		this.limitPrice = limitPrice;
+		this.stopPrice = stopPrice;
+		this.timeInForce = timeInForce;
+		this.state = state;
+	}
+	
+	public OrderAction getAction()
+	{
+		return this.action;
+	}
+	
+	public String getId()
+	{
+		return this.id;
+	}
+	
+	public double getLimitPrice()
+	{
+		return this.limitPrice;
+	}
+	
+	public float getQuantity()
+	{
+		return this.quantity;
+	}
+	
+	public OrderState getState()
+	{
+		return this.state;
+	}
+	
+	public Stock getStock()
+	{
+		return this.stock;
+	}
+	
+	public double getStopPrice()
+	{
+		return this.stopPrice;
+	}
+	
+	public TimeInForce getTimeInForce()
+	{
+		return this.timeInForce;
+	}
+	
+	public OrderType getType()
+	{
+		return this.type;
+	}
+	
+	public String getUserName()
+	{
+		return this.userName;
+	}
+	
+	public void setState(OrderState state)
+	{
+		this.state = state;
+	}
 }
