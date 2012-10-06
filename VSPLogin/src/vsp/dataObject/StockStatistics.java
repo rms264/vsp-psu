fa96264e6b8e47e48d0db5249ac51d8c02ac8ff6
Jason <jmt9n@alumni.virginia.edu>
2012-10-06 13:35:39 -0400

    Basic implementation

--- VSPLogin/src/vsp/dataObject/StockStatistics.java
+++ VSPLogin/src/vsp/dataObject/StockStatistics.java
@@ -2,5 +2,44 @@ package vsp.dataObject;
 
 public final class StockStatistics
 {
-	// TODO: implement
+	private double returnOnInvestment;
+	private double compoundRateOfReturn;
+	private double compoundAnnualGrowthRate;
+	private double averageRateOfReturn;
+	private double standardDeviation;
+	
+	public StockStatistics()
+	{
+		// no implementation required
+	}
+	
+	public void calculate(HistoricalStockInfo data)
+	{
+		// TODO: implement
+	}
+
+	public double getAverageRateOfReturn()
+	{
+		return this.averageRateOfReturn;
+	}
+	
+	public double getCompoundAnnualGrowthRate()
+	{
+		return this.compoundAnnualGrowthRate;
+	}
+	
+	public double getCompouandRateOfReturn()
+	{
+		return this.compoundRateOfReturn;
+	}
+	
+	public double getReturnOnInvestment()
+	{
+		return this.returnOnInvestment;
+	}
+	
+	public double getStandardDeviation()
+	{
+		return this.standardDeviation;
+	}
 }
