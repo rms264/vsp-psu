7915128d7e111d9cae376593cdde114aa0f098ff
Jason <jmt9n@alumni.virginia.edu>
2012-10-06 17:18:00 -0400

    Added stub methods.

--- VSPLogin/src/vsp/StockInfoServiceProvider.java
+++ VSPLogin/src/vsp/StockInfoServiceProvider.java
@@ -1,6 +1,32 @@
 package vsp;
 
+import java.util.List;
+
+import vsp.dataObject.HistoricalStockInfo;
+import vsp.dataObject.StockInfo;
+
 public final class StockInfoServiceProvider
 {
-	// TODO: implement
+	public StockInfoServiceProvider()
+	{
+		// no implementation required
+	}
+	
+	public StockInfo requestCurrentStockData(String symbol)
+	{
+		// TODO: implement
+		return null;
+	}
+	
+	public List<StockInfo> requestCurrentStockData(List<String> symbols)
+	{
+		// TODO: implement
+		return null;
+	}
+	
+	public HistoricalStockInfo requestHistoricalStockData(String symbol, int months)
+	{
+		// TODO: implement
+		return null;
+	}
 }
