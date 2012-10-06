ea636ee1868b048f9dde79bed68f82bf96aaae16
Jason <jmt9n@alumni.virginia.edu>
2012-10-06 13:30:52 -0400

    Implemented

--- VSPLogin/src/vsp/orders/OrderResult.java
+++ VSPLogin/src/vsp/orders/OrderResult.java
@@ -1,6 +1,47 @@
 package vsp.orders;
 
+import java.util.Date;
+
 public final class OrderResult
 {
-	// TODO: implement
+	private final Order order;
+	private final boolean completed;
+	private final float quantity;
+	private final double sharePrice;
+	private final Date dateTime;
+	
+	public OrderResult(Order order, boolean completed, float quantity, 
+			double sharePrice, Date dateTime)
+	{
+		this.order = order;
+		this.completed = completed;
+		this.quantity = quantity;
+		this.sharePrice = sharePrice;
+		this.dateTime = dateTime;
+	}
+	
+	public boolean getCompleted()
+	{
+		return this.completed;
+	}
+	
+	public Date getDateTime()
+	{
+		return this.dateTime;
+	}
+	
+	public Order getOrder()
+	{
+		return this.order;
+	}
+	
+	public float getQuantity()
+	{
+		return this.quantity;
+	}
+	
+	public double getSharePrice()
+	{
+		return this.sharePrice;
+	}
 }
