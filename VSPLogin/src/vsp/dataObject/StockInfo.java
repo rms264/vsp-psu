e8fbf4d02b2b04b4c451db807908424212589f7e
Jason <jmt9n@alumni.virginia.edu>
2012-10-06 18:11:51 -0400

    Added members.

--- VSPLogin/src/vsp/dataObject/StockInfo.java
+++ VSPLogin/src/vsp/dataObject/StockInfo.java
@@ -1,6 +1,90 @@
 package vsp.dataObject;
 
+import java.util.Date;
+
 public final class StockInfo
 {
-	// TODO: populate
+	private final String symbol;
+	private final String description;
+	private final double dayHigh;
+	private final double dayLow;
+	private final Date exdividendDate;
+	private final double exdividend;
+	private final double priceChangeSinceOpen;
+	private final double percentChangeSinceOpen;
+	private final int volume;
+	private final double bid;
+	private final double open;
+	
+	public StockInfo(String symbol, String description, double dayHigh, double dayLow, 
+			Date exdividendDate, double exdividend, double priceChangeSinceOpen, 
+			double percentChangeSinceOpen, int volume, double bid, double open)
+	{
+		this.symbol = symbol;
+		this.description = description;
+		this.dayHigh = dayHigh;
+		this.dayLow = dayLow;
+		this.exdividendDate = exdividendDate;
+		this.exdividend = exdividend;
+		this.priceChangeSinceOpen = priceChangeSinceOpen;
+		this.percentChangeSinceOpen = percentChangeSinceOpen;
+		this.volume = volume;
+		this.bid = bid;
+		this.open = open;
+	}
+	
+	public double getBid()
+	{
+		return this.bid;
+	}
+	
+	public double getDayHigh()
+	{
+		return this.dayHigh;
+	}
+	
+	public double getDayLow()
+	{
+		return this.dayLow;
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
+	}
+	
+	public double getExdividend()
+	{
+		return this.exdividend;
+	}
+	
+	public Date getExdividendDate()
+	{
+		return this.exdividendDate;
+	}
+	
+	public double getOpen()
+	{
+		return this.open;
+	}
+	
+	public double getPercentChangeSinceOpen()
+	{
+		return this.percentChangeSinceOpen;
+	}
+	
+	public double getPriceChanceSinceOpen()
+	{
+		return this.priceChangeSinceOpen;
+	}
+	
+	public String getSymbol()
+	{
+		return this.symbol;
+	}
+	
+	public int getVolume()
+	{
+		return this.volume;
+	}
 }
