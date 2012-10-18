1a1858bc2ef20740b30349e7f48ccb0ef919e46f
Jason <jmt9n@alumni.virginia.edu>
2012-10-17 20:43:44 -0400

    Limit order logic completed.

--- VSPLogin/src/vsp/dataObject/LimitOrderExecutor.java
+++ VSPLogin/src/vsp/dataObject/LimitOrderExecutor.java
@@ -1,16 +1,145 @@
 package vsp.dataObject;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import vsp.utils.Enumeration.OrderAction;
+import vsp.utils.Enumeration.TimeInForce;
+
 final class LimitOrderExecutor extends OrderExecutor
 {
+	private final Date today;
+	
 	LimitOrderExecutor()
 	{
-		// no implementation required
+		Calendar currDtCal = Calendar.getInstance();
+
+	    // Zero out the hour, minute, second, and millisecond
+	    currDtCal.set(Calendar.HOUR_OF_DAY, 0);
+	    currDtCal.set(Calendar.MINUTE, 0);
+	    currDtCal.set(Calendar.SECOND, 0);
+	    currDtCal.set(Calendar.MILLISECOND, 0);
+	    
+		this.today = currDtCal.getTime();
 	}
 	
 	@Override
 	public OrderResult Execute(Order order, IUserBalance balanceService, IStockInfo stockService)
 	{
-		// TODO: implement
-		return null;
+		OrderResult result = new OrderResult(order);
+		Date submitted = order.getDateSubmitted();
+		if (submitted.getYear() == today.getYear() 
+				&& submitted.getMonth() == today.getMonth()
+				&& submitted.getDay() == today.getDay())
+		{
+			StockInfo info = stockService.requestCurrentStockData(order.getStock().getStockSymbol());
+			if (info != null)
+			{
+				attemptTrade(result, balanceService, stockService, today, info.getDayLow(), info.getDayHigh(), info.getVolume());
+			}
+		}
+		else
+		{
+			List<HistoricalStockInfo> infos = stockService.requestDailyHistoricalStockData(order.getStock().getStockSymbol(), order.getLastEvaluated());
+			if (infos != null && infos.size() > 0)
+			{
+				HistoricalStockInfo info;
+				for (int i = infos.size() - 1; i >= 0; --i)
+				{
+					info = infos.get(i);
+					attemptTrade(result, balanceService, stockService, info.getDate(), info.getDayLow(), info.getDayHigh(), info.getVolume());
+					if (result.getCompleted() || result.getCancelled())
+					{
+						break;
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private void attemptTrade(OrderResult result, IUserBalance balanceService, IStockInfo stockService, Date date, double dayLow, double dayHigh, int volume)
+	{
+		Order order = result.getOrder();
+		
+		float quantity = order.getQuantity();
+		if (quantity > volume)
+		{
+			return;
+		}
+				
+		
+		try
+		{
+			double accountBalance = balanceService.getBalance(order.getUserName());
+			if (order.getAction() == OrderAction.BUY)
+			{
+				if (order.getLimitPrice() < dayLow)
+				{ // price was too high to buy today
+					return;
+				}
+				
+				double orderTotal = quantity * dayLow;
+				if (accountBalance >= orderTotal)
+				{
+					try
+					{
+						balanceService.updateBalance(order.getUserName(), accountBalance - orderTotal);
+						result.setCompleted(true);
+						result.setQuantity(quantity);
+						result.setSharePrice(dayLow);
+						result.setDateTime(date);
+					}
+					catch (Exception ex)
+					{
+						// ignore
+					}
+				}
+			}
+			else
+			{
+				if (order.getLimitPrice() > dayHigh)
+				{  // price was too low to sell today
+					return;
+				}
+				
+				double orderTotal = quantity * dayHigh;
+				try
+				{
+					balanceService.updateBalance(order.getUserName(), accountBalance + orderTotal);
+					result.setCompleted(true);
+					result.setQuantity(quantity);
+					result.setSharePrice(dayHigh);
+					result.setDateTime(date);
+				}
+				catch (Exception ex)
+				{
+					// ignore
+				}
+			}
+		}
+		finally
+		{
+			// these apply on the first historical trade attempt (should be the same day the order was submitted)
+			if (!result.getCompleted())
+			{
+				if (order.getTimeInForce() == TimeInForce.DAY 
+						&& (today.after(date) || (today.equals(date) && !stockService.isWithinTradingHours())))
+				{ // Cancel order
+					result.setCancelled(true);
+					result.setDateTime(date);
+				}
+				
+				if ((order.getTimeInForce() == TimeInForce.IMMEDIATEORCANCEL || order.getTimeInForce() == TimeInForce.FILLORKILL))
+				{ // Cancel order
+					result.setCancelled(true);
+					result.setDateTime(date);
+				}
+			}
+			
+			// GOOD UNTIL CANCELLED lives on for another day
+		}
 	}
 }
