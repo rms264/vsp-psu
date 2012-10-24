0472782711219170cded2a2fb163bf0f7fef7faf
Jason <jmt9n@alumni.virginia.edu>
2012-10-23 21:17:49 -0400

    Ordered the unit tests & added some comments.

--- VSPLogin/src/unitTests/StockTransactions.java
+++ VSPLogin/src/unitTests/StockTransactions.java
@@ -10,6 +10,10 @@ import org.junit.*;
 import org.junit.runner.*;
 import vsp.*;
 import vsp.dal.requests.Orders;
+import vsp.dal.requests.PortfolioEntries;
+import vsp.dal.requests.Stocks;
+import vsp.dal.requests.Transactions;
+import vsp.dal.requests.Users;
 import vsp.dataObject.DividendInfo;
 import vsp.dataObject.HistoricalStockInfo;
 import vsp.dataObject.Order;
@@ -28,6 +32,41 @@ public class StockTransactions
 	private final SimpleDateFormat historicalDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 	private final VspServiceProvider vsp = new VspServiceProvider();
 	private final StockInfoServiceProvider stockInfoSP = new StockInfoServiceProvider(); 
+	private final static String userName = "test";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		// delete all stocks (should delete all portfolio entries, orders and transactions too because of the DB constraints)
+		List<Stock> stocks = Stocks.getAllStocks();
+		for (Stock stock : stocks)
+		{
+			Stocks.deleteStock(stock.getStockSymbol());
+		}
+		
+		// ensure all stocks are gone
+		stocks = Stocks.getAllStocks();
+		Assert.assertNotNull(stocks);
+		Assert.assertTrue(stocks.size() == 0);
+		
+		// ensure all owned stocks are gone
+		List<PortfolioData> ownedItems = PortfolioEntries.requestAllUserStocks(userName);
+		Assert.assertNotNull(ownedItems);
+		Assert.assertTrue(ownedItems.size() == 0);
+		
+		// ensure all orders are gone
+		List<Order> orders = Orders.getOrdersForUser(userName);
+		Assert.assertNotNull(orders);
+		Assert.assertTrue(orders.size() == 0);
+		
+		// ensure all transactions are gone
+		List<StockTransaction> transactions = Transactions.getTransactionsForUser(userName);
+		Assert.assertNotNull(transactions);
+		Assert.assertTrue(transactions.size() == 0);
+		
+		// set user balance to 0
+		Users.updateBalance(userName, 0.0);
+	}
 	
 	@Test
 	@unitTests.Order(order=1)
@@ -83,75 +122,60 @@ public class StockTransactions
 		Assert.assertTrue(dividendResults.size() == 0);
 	}
 	
-	@Test	
+	@Test
+	@unitTests.Order(order=4)
 	public void showUserTransactionHistory()  throws Exception
 	{		
 		// TODO:  ensure there is at least one transaction before calling this unit test (or add some default ones to database)
-		List<StockTransaction> transactions = vsp.getTransactionHistory("test");
+		List<StockTransaction> transactions = vsp.getTransactionHistory(userName);
 		Assert.assertNotNull("Unable to retrieve transaction history.", transactions);
 		Assert.assertTrue(transactions.size() > 0);
 	}
 	
-	@Test	
-	public void displayUserPortfolio() throws Exception
-	{
-		Assert.fail("This test must be performed manually.");
-	}
-	
-	@Test	
-	public void showUserPendingOrdersWhenSomeExist() throws Exception
-	{
-		// TODO:  ensure that at least one order has been submitted that will remain in a pending state for a few seconds
-		List<Order> orders = vsp.getPendingOrders("test");
-		Assert.assertNotNull("Unable to retrieve pending orders.", orders);
-		Assert.assertTrue(orders.size() > 0);
-	}
-	
-	@Test	
+	@Test
+	@unitTests.Order(order=5)
 	public void showUserPendingOrdersWhenNoneExist() throws Exception
 	{
-		// TODO: ensure no pending orders are in the system
-		List<Order> orders = vsp.getPendingOrders("test");
+		List<Order> orders = vsp.getPendingOrders(userName);
 		Assert.assertNotNull("Unable to retrieve pending orders.", orders);
 		Assert.assertTrue(orders.size() == 0);
 	}
 	
-	@Test	
-	public void cancelPendingOrder() throws Exception
+	@Test
+	@unitTests.Order(order=6)
+	public void buyStockInsufficientFunds()
 	{
-		String userName = "test";
-		List<Order> orders = vsp.getPendingOrders(userName);
-		Assert.assertNotNull("Unable to retrieve pending orders.", orders);
-		Assert.assertTrue(orders.size() > 0);
-		
-		Order order = orders.get(0);
-		vsp.cancelOrder(userName, order.getId());
-		
-		order = Orders.getOrderById(order.getId());
-		Assert.assertTrue(order.getState() == OrderState.CANCELLED);
+		// TODO: implement
+		Assert.fail("May need to be executed manually, as sufficient funds are calculated in the JSP (or we can move to createOrder method in VSP).");
 	}
 	
-	@Test	
+	@Test
+	@unitTests.Order(order=7)
+	public void buyMarketOrderStockInsufficientFunds() throws Exception
+	{
+		// TODO: implement
+		Assert.fail("May need to be executed manually, as sufficient funds are calculated in the JSP (or we can move to createOrder method in VSP).");
+		
+		// return funds now that "insufficient" tests are over
+		Users.updateBalance(userName, Users.DEFAULT_BALANCE);
+	}
+	
+	@Test
+	@unitTests.Order(order=8)
 	public void buyStock()
 	{
 		// TODO: implement
 		Assert.fail("Not yet implemented");
 	}
 	
-	@Test	
-	public void buyStockInsufficientFunds()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
 	@Test
+	@unitTests.Order(order=9)
 	public void buyMarketOrderStock() throws Exception
 	{
-		Order order = vsp.createOrder("test", 
+		Order order = vsp.createOrder(userName, 
 				OrderAction.BUY.toString(), 
-				"AAPL", 
-				"10", 
+				"MSFT", 
+				"100", 
 				OrderType.MARKET.toString(),
 				TimeInForce.DAY.toString(), // only DAY is supported for a MARKET order
 				"0.0",
@@ -160,15 +184,9 @@ public class StockTransactions
 		Assert.assertNotNull("Unable to create order.", order);
 		Assert.assertNotNull("Order ID is null.", order.getId());
 	}
-	
+		
 	@Test
-	public void buyMarketOrderStockInsufficientFunds()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test
+	@unitTests.Order(order=10)
 	public void buyLimitOrderStockLimitPriceMet()
 	{
 		// TODO: implement
@@ -176,6 +194,7 @@ public class StockTransactions
 	}
 	
 	@Test
+	@unitTests.Order(order=11)
 	public void buyLimitOrderStockLimitPriceNotMet()
 	{
 		// TODO: implement
@@ -183,6 +202,7 @@ public class StockTransactions
 	}
 	
 	@Test
+	@unitTests.Order(order=12)
 	public void buyStopOrderStockStopPriceMet()
 	{
 		// TODO: implement
@@ -190,6 +210,7 @@ public class StockTransactions
 	}
 	
 	@Test
+	@unitTests.Order(order=13)
 	public void buyStopOrderStockStopPriceNotMet()
 	{
 		// TODO: implement
@@ -197,6 +218,7 @@ public class StockTransactions
 	}
 	
 	@Test
+	@unitTests.Order(order=14)
 	public void buyStopLimitOrderStockStopPriceMet()
 	{
 		// TODO: implement
@@ -204,78 +226,15 @@ public class StockTransactions
 	}
 	
 	@Test
+	@unitTests.Order(order=15)
 	public void buyStopLimitOrderStockStopPriceNotMet()
 	{
 		// TODO: implement
 		Assert.fail("Not yet implemented");
 	}
 	
-	
-	@Test	
-	public void sellStock()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockMoreThanOwned()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockMarketOrder()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockLimitOrderLimitPriceMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockLimitOrderLimitPriceNotMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockStopLossOrderStopPriceMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockStopLossOrderStopPriceNotMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockStopLimitOrderStopPriceMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	@Test	
-	public void sellStockStopLimitOrderStopPriceNotMet()
-	{
-		// TODO: implement
-		Assert.fail("Not yet implemented");
-	}
-	
-	
-	@Test	
+	@Test
+	@unitTests.Order(order=16)
 	public void dayOrderExecution()
 	{
 		// TODO: implement
@@ -283,6 +242,7 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=17)
 	public void dayOrderExpired()
 	{
 		// TODO: implement
@@ -290,6 +250,7 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=18)
 	public void goodUntilCancelledExecution()
 	{
 		// TODO: implement
@@ -297,6 +258,7 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=19)
 	public void goodUntilCancelledExpired()
 	{
 		// TODO: implement
@@ -304,6 +266,7 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=20)
 	public void fillOrKillFilled()
 	{
 		// TODO: implement
@@ -311,6 +274,7 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=21)
 	public void fillOrKillKilled()
 	{
 		// TODO: implement
@@ -318,7 +282,114 @@ public class StockTransactions
 	}
 	
 	@Test	
+	@unitTests.Order(order=22)
 	public void immediateOrCancel()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=23)
+	public void displayUserPortfolio() throws Exception
+	{
+		// TODO: implement (user must own some stuff first)
+		Assert.fail("May need to be executed manually");
+	}
+	
+	@Test
+	@unitTests.Order(order=24)
+	public void showUserPendingOrdersWhenSomeExist() throws Exception
+	{
+		// TODO:  ensure that at least one order has been submitted that will remain in a pending state for a few seconds
+		List<Order> orders = vsp.getPendingOrders(userName);
+		Assert.assertNotNull("Unable to retrieve pending orders.", orders);
+		Assert.assertTrue(orders.size() > 0);
+	}
+	
+	@Test
+	@unitTests.Order(order=25)
+	public void cancelPendingOrder() throws Exception
+	{
+		List<Order> pendingOrders = vsp.getPendingOrders(userName);
+		Assert.assertNotNull("Unable to retrieve pending orders.", pendingOrders);
+		Assert.assertTrue(pendingOrders.size() > 0);
+		
+		Order order = pendingOrders.get(0);
+		Assert.assertTrue(order.getState() == OrderState.PENDING);
+		vsp.cancelOrder(userName, order.getId());
+		
+		order = Orders.getOrderById(order.getId());
+		Assert.assertTrue(order.getState() == OrderState.CANCELLED);
+	}
+	
+	@Test
+	@unitTests.Order(order=26)
+	public void sellStockMoreThanOwned()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=27)
+	public void sellStock()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+		
+	@Test
+	@unitTests.Order(order=28)
+	public void sellStockMarketOrder()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=29)
+	public void sellStockLimitOrderLimitPriceMet()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=30)
+	public void sellStockLimitOrderLimitPriceNotMet()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=31)
+	public void sellStockStopLossOrderStopPriceMet()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=32)
+	public void sellStockStopLossOrderStopPriceNotMet()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=33)
+	public void sellStockStopLimitOrderStopPriceMet()
+	{
+		// TODO: implement
+		Assert.fail("Not yet implemented");
+	}
+	
+	@Test
+	@unitTests.Order(order=34)
+	public void sellStockStopLimitOrderStopPriceNotMet()
 	{
 		// TODO: implement
 		Assert.fail("Not yet implemented");
