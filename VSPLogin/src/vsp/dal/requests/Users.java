58c0820d858f9b539dcb8a9779a433b5f3236cb3
Jason <jmt9n@alumni.virginia.edu>
2012-10-23 21:05:04 -0400

    Made default balance a publicly accessible item.

--- VSPLogin/src/vsp/dal/requests/Users.java
+++ VSPLogin/src/vsp/dal/requests/Users.java
@@ -20,7 +20,7 @@ import vsp.utils.Enumeration.SecurityQuestion;
 
 public class Users
 {
-	private static final double DEFAULT_BALANCE = 20000.0; // $20,000 USD
+	public static final double DEFAULT_BALANCE = 20000.0; // $20,000 USD
 	
 	public static boolean addTraderAccount(String userName, String email, 
 			String password1, String password2, String questionNum, 
