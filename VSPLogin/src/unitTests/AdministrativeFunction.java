164a1fbbc62a2f2cd179bae6f7fbb94c2ae5d64a
Ravi <rsm5253@psu.edu>
2012-11-20 11:59:10 -0500

    Updated Unit Test Cases

--- VSPLogin/src/unitTests/AdministrativeFunction.java
+++ VSPLogin/src/unitTests/AdministrativeFunction.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import vsp.VspServiceProvider;
+import vsp.utils.Validate;
 
 @RunWith(OrderedRunner.class)
 public class AdministrativeFunction
@@ -38,13 +39,52 @@ public class AdministrativeFunction
 	@Order(order=2)
 	public void changePasswordInvalid()
 	{
-		fail("Not yet implemented");		
+		try
+		{
+			// the default traders in the database include 'test' and 'test1'
+			List<String> traders = vsp.getTraders();
+			Assert.assertTrue(traders.size() >= 2);
+			Assert.assertTrue(traders.contains("test"));			
+			Assert.assertTrue(Validate.validatePassword("test", "Test123", "Test123"));
+			Assert.assertTrue(vsp.updateUserPassword("test", "Test123", "Test123"));
+			Assert.fail();
+		}
+		catch (Exception e)
+		{
+			Assert.assertTrue(e.toString().contains("The password must contain at least 8 characters."));
+		}		
 	}
 	
 	@Test
 	@Order(order=3)
 	public void changePasswordValid()
 	{
-		fail("Not yet implemented");		
+		try
+		{
+			// the default traders in the database include 'test' and 'test1'
+			List<String> traders = vsp.getTraders();
+			Assert.assertTrue(traders.size() >= 2);
+			Assert.assertTrue(traders.contains("test"));			
+			Assert.assertTrue(Validate.validatePassword("test", "Test12345", "Test12345"));
+			Assert.assertTrue(vsp.updateUserPassword("test", "Test12345", "Test12345"));			
+		}
+		catch (Exception e)
+		{
+			Assert.fail();
+		}		
+	}
+	
+	@Test
+	@Order(order=4)
+	public void resetPasswordValid()
+	{
+		try
+		{
+			Assert.assertTrue(vsp.updateUserPassword("test", "Test1234", "Test1234"));			
+		}
+		catch (Exception e)
+		{
+			Assert.fail();
+		}		
 	}
 }
